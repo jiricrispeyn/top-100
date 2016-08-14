918203ee4e80172ca60a503bb045b9fdbe67257a
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2016-08-14 16:16:49 +0200

    Add playlist to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { PlaylistComponent } from './playlist';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [PlaylistComponent]
 })
 export class AppComponent {
-  title = 'app works!';
+  constructor(af: AngularFire) {
+
+  }
+
+  title = 'Top100';
 }
