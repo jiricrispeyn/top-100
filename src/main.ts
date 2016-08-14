98644e59a3002dbda6ea774d1a1e56793984477a
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2016-08-14 15:10:36 +0200

    Specify Firebase configurations

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import { FIREBASE_PROVIDERS, defaultFirebase } from 'angularfire2';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  FIREBASE_PROVIDERS,
+  defaultFirebase({
+    apiKey: 'AIzaSyCuB1APdu_P4Pxyb0S9Lw9RTb_vMwohBaA',
+    authDomain: 'top-100-97d6e.firebaseapp.com',
+    databaseURL: 'https://top-100-97d6e.firebaseio.com',
+    storageBucket: 'top-100-97d6e.appspot.com'
+  })
+]);
