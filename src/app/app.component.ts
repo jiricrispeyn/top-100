9de8769b2daa50542f87ca5d538d145004ba9eb9
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2016-09-01 00:24:03 +0200

    Provide router directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { ROUTER_DIRECTIVES } from '@angular/router';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
-import { PlaylistComponent } from './playlist';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [PlaylistComponent]
+  directives: [ROUTER_DIRECTIVES]
 })
 export class AppComponent {
   constructor(af: AngularFire) {
