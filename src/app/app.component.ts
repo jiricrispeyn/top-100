df5b12aec9285527035f37cff5c7cdaa7709f5ee
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2016-09-04 22:22:52 +0200

    Add provider for Spotify service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { ROUTER_DIRECTIVES } from '@angular/router';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { SpotifyService } from './shared';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ROUTER_DIRECTIVES]
+  providers: ['SpotifyService'],
+  directives: [ROUTER_DIRECTIVES, HTTP_PROVIDERS]
 })
 export class AppComponent {
   constructor(af: AngularFire) {
