0ed07e9a007026e7d48fd9886aa38ca9e7dbd063
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2016-09-04 22:10:43 +0200

    Assign type RouterConfig to AppRoutes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
+import { RouterConfig } from '@angular/router';
+
 import { PlaylistComponent } from './playlist/playlist.component';
 import { EntriesComponent } from './entries/entries.component';
 import { AddEntryComponent } from './entries/add-entry/add-entry.component';
 
-export const AppRoutes = [
+export const AppRoutes: RouterConfig = [
   { path: 'playlist', component: PlaylistComponent },
   { path: 'entries', component: EntriesComponent },
   { path: 'entries/add', component: AddEntryComponent },
-  { path: '', redirectTo: '/playlist', pathMatch: 'full' }
+  { path: '', redirectTo: 'playlist', pathMatch: 'full' }
 ]
