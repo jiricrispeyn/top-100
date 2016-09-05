70b99c73785d14fc525226d1bad7b22e04ab8226
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2016-09-06 00:40:41 +0200

    Get playlist from database and visualize it

--- src/app/playlist/playlist.component.ts
+++ src/app/playlist/playlist.component.ts
@@ -10,15 +10,42 @@ import { AngularFire, FirebaseListObservable } from 'angularfire2';
 export class PlaylistComponent implements OnInit {
 
   entries: FirebaseListObservable<any[]>;
+  playlist = [];
 
   constructor(private af: AngularFire) { }
 
   ngOnInit() {
     this.entries = this.af.database.list('/entries');
 
-    this.entries.subscribe(
-      res => console.log(res)
-    );
+    this.entries.map(
+      res => {
+        // get unique entries from database
+        let uniqueEntries = _.uniq(res, uniqueEntry => {
+          return JSON.stringify(_.pick(uniqueEntry, ['song', 'artist']));
+        });
+
+        uniqueEntries = uniqueEntries.map(uniqueEntry => {
+          // get all entries related to unique entry
+          let relatedEntries = res.filter((relatedEntry: any) => {
+            return uniqueEntry.song === relatedEntry.song && uniqueEntry.artist === relatedEntry.artist;
+          });
+
+          return _.extend({}, uniqueEntry, {
+            // replace score with score count of related entries
+            score: _.reduce(relatedEntries, (memo, num: any) => {
+              return memo + num.score;
+            }, 0)
+          });
+        });
+
+        // sort asc by score
+        return uniqueEntries.sort((a, b) => b.score - a.score);
+      }
+    ).subscribe(
+      res => {
+        // only show first 100 entries
+        this.playlist = res.slice(0, 100);
+    });
   }
 
 }
