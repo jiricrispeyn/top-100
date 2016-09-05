3a2b316b110a4570603aeb662f6dd377d5eb56ca
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2016-09-06 00:45:04 +0200

    new gh-pages version

---
+++ system-config.js
@@ -0,0 +1,65 @@
+"use strict";
+// SystemJS configuration file, see links for more information
+// https://github.com/systemjs/systemjs
+// https://github.com/systemjs/systemjs/blob/master/docs/config-api.md
+/***********************************************************************************************
+ * User Configuration.
+ **********************************************************************************************/
+/** Map relative paths to URLs. */
+var map = {
+    'ng2-bootstrap': 'vendor/ng2-bootstrap',
+    'firebase': 'vendor/firebase/firebase.js',
+    'angularfire2': 'vendor/angularfire2'
+};
+/** User packages configuration. */
+var packages = {
+    'ng2-bootstrap': {
+        format: 'cjs',
+        defaultExtension: 'js',
+        main: 'ng2-bootstrap.js'
+    },
+    'angularfire2': {
+        defaultExtension: 'js',
+        main: 'angularfire2.js'
+    }
+};
+////////////////////////////////////////////////////////////////////////////////////////////////
+/***********************************************************************************************
+ * Everything underneath this line is managed by the CLI.
+ **********************************************************************************************/
+var barrels = [
+    // Angular specific barrels.
+    '@angular/core',
+    '@angular/common',
+    '@angular/compiler',
+    '@angular/forms',
+    '@angular/http',
+    '@angular/router',
+    '@angular/platform-browser',
+    '@angular/platform-browser-dynamic',
+    // Thirdparty barrels.
+    'rxjs',
+    // App specific barrels.
+    'app',
+    'app/shared',
+    'app/playlist',
+    'app/playlist/add-entry',
+    'app/entries',
+    'app/entries/add-entry',
+];
+var cliSystemConfigPackages = {};
+barrels.forEach(function (barrelName) {
+    cliSystemConfigPackages[barrelName] = { main: 'index' };
+});
+// Apply the CLI SystemJS configuration.
+System.config({
+    map: {
+        '@angular': 'vendor/@angular',
+        'rxjs': 'vendor/rxjs',
+        'main': 'main.js'
+    },
+    packages: cliSystemConfigPackages
+});
+// Apply the user's configuration.
+System.config({ map: map, packages: packages });
+//# sourceMappingURL=system-config.js.map
